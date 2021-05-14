99e1bf3090ced34d3f202167f3e61858c6dc1943
한영인 <gksduddls33@gmail.com>
2021-05-15 04:02:16 +0900

    0.0.2 이미지 및 버튼 생성
    아래에 만들었음

--- MainGuI.py
+++ MainGuI.py
@@ -11,16 +11,28 @@ class MainGui:
         self.Button_Bugyoung = Button(self.MainWnd, text='부경', width=10, height=5, command=self.Button_Bugyoung_Input)
         self.Button_Jeju     = Button(self.MainWnd, text='제주', width=10, height=5, command=self.Button_Jeju_Input)
 
+        self.photoGmail = PhotoImage(file="Photo/Gmail_icon.png")
+        self.Button_Gmail = Button(self.MainWnd, image=self.photoGmail, command=self.Button_Gmail_Send)
+        self.photoTelegram = PhotoImage(file="Photo/Telegram_icon.png")
+        self.Button_Telegram = Button(self.MainWnd, image=self.photoTelegram, command=self.Button_Telegram_Send)
+        self.photoNamuwiki = PhotoImage(file="Photo/Namuwiki_icon.png")
+        self.Button_Namuwiki = Button(self.MainWnd, image=self.photoNamuwiki, command=self.Button_Namuwiki_Link)
+
         self.Button_Seoul.place(x=200, y=50)
         self.Button_Bugyoung.place(x=200, y=200)
         self.Button_Jeju.place(x=200, y=350)
-
+        self.Button_Gmail.place(x=0, y=580)
+        self.Button_Telegram.place(x=380, y=580)
+        self.Button_Namuwiki.place(x=190, y=580)
         self.MainWnd.mainloop()
 
     def Button_Seoul_Input(self):
         self.Button_Seoul.destroy()
         self.Button_Bugyoung.destroy()
         self.Button_Jeju.destroy()
+        self.Button_Gmail.destroy()
+        self.Button_Telegram.destroy()
+        self.Button_Namuwiki.destroy()
 
         self.TurnToSearchScene()
 
@@ -28,6 +40,9 @@ class MainGui:
         self.Button_Seoul.destroy()
         self.Button_Bugyoung.destroy()
         self.Button_Jeju.destroy()
+        self.Button_Gmail.destroy()
+        self.Button_Telegram.destroy()
+        self.Button_Namuwiki.destroy()
 
         self.TurnToSearchScene()
 
@@ -35,8 +50,20 @@ class MainGui:
         self.Button_Seoul.destroy()
         self.Button_Bugyoung.destroy()
         self.Button_Jeju.destroy()
+        self.Button_Gmail.destroy()
+        self.Button_Telegram.destroy()
+        self.Button_Namuwiki.destroy()
 
         self.TurnToSearchScene()
+
+    def Button_Gmail_Send(self):
+        pass
+
+    def Button_Telegram_Send(self):
+        pass
+
+    def Button_Namuwiki_Link(self):
+        pass
 
     def TurnToSearchScene(self):
         pass
