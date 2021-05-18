2f90ef72dc692f0d4c82839531d439eead019841
Hongsom98 <71174760+Hongsom98@users.noreply.github.com>
2021-05-18 09:41:37 +0900

    0.0.6 씬전환 함수화

--- MainGuI.py
+++ MainGuI.py
@@ -6,6 +6,7 @@ import XmlProcess
 def Button_Namuwiki_Link():
     webbrowser.open("https://namu.wiki/w/%EA%B2%BD%EB%A7%88")
 
+
 class MainGui:
     def __init__(self):
         self.MainWnd = Tk()
@@ -14,10 +15,13 @@ class MainGui:
         self.MainWnd.resizable(False, False)
 
         self.MainWnd_Button_List = []
+        self.SearchObjectsList = []
 
         self.photoGmail = PhotoImage(file="Photo/Gmail_icon.png")
         self.photoTelegram = PhotoImage(file="Photo/Telegram_icon.png")
         self.photoNamuwiki = PhotoImage(file="Photo/Namuwiki_icon.png")
+
+        self.input_text = None
 
         self.MainSceneButtons()
 
@@ -25,9 +29,12 @@ class MainGui:
 
     def MainSceneButtons(self):
 
-        self.MainWnd_Button_List.append(Button(self.MainWnd, text='서울', width=10, height=5, command=self.ButtonSeoulInput))
-        self.MainWnd_Button_List.append(Button(self.MainWnd, text='부경', width=10, height=5, command=self.ButtonBugyoungInput))
-        self.MainWnd_Button_List.append(Button(self.MainWnd, text='제주', width=10, height=5, command=self.ButtonJejuInput))
+        self.MainWnd_Button_List.append(
+            Button(self.MainWnd, text='서울', width=10, height=5, command=self.ButtonSeoulInput))
+        self.MainWnd_Button_List.append(
+            Button(self.MainWnd, text='부경', width=10, height=5, command=self.ButtonBugyoungInput))
+        self.MainWnd_Button_List.append(
+            Button(self.MainWnd, text='제주', width=10, height=5, command=self.ButtonJejuInput))
 
         self.MainWnd_Button_List.append(Button(self.MainWnd, image=self.photoGmail, command=self.ButtonGmailSend))
         self.MainWnd_Button_List.append(Button(self.MainWnd, image=self.photoTelegram, command=self.ButtonTelegramSend))
@@ -43,60 +50,60 @@ class MainGui:
         self.MainWnd_Button_List[4].place(x=380, y=580)
         self.MainWnd_Button_List[5].place(x=190, y=580)
 
+    def SetSearchButtons(self):
+        self.input_text = StringVar()
+        self.SearchObjectsList.append(Entry(self.MainWnd, textvariable=self.input_text, width=30))
+
+        self.SearchObjectsList.append(Button(self.MainWnd, text="검색", command=self.SearchDef))
+
+        self.SearchObjectsPlace()
+
+    def SearchObjectsPlace(self):
+        self.SearchObjectsList[0].place(x=0, y=110)
+        self.SearchObjectsList[1].place(x=250, y=110)
+
     def SearchDef(self):
         Result = XmlProcess.SearchHorseProfile(self.input_text.get())
         for key, value in Result.items():
             print(key, ":", value)
 
-    def ButtonSeoulInput(self):
+
+    def TurnToSearchScene(self):
         for i in range(len(self.MainWnd_Button_List)):
             self.MainWnd_Button_List[i].destroy()
-        self.input_text = StringVar()
-        Entry(self.MainWnd,textvariable = self.input_text, width = 30).place(x=0,y=110)
+        self.MainWnd_Button_List.clear()
 
-        self.SearhButton = Button(self.MainWnd,text="검색",command=self.SearchDef)
-        self.SearhButton.place(x=250,y=110)
+        self.SetSearchButtons()
 
+    def TurnToMainScene(self):
+        for i in range(len(self.SearchObjectsList)):
+            self.SearObjectsList.destroy()
+        self.SearchObjectsList.clear()
+
+        self.MainSceneButtons()
+
+
+    def ButtonSeoulInput(self):
         XmlProcess.SearchLegion = "1"
-
 
         self.TurnToSearchScene()
 
-
     def ButtonBugyoungInput(self):
-        for i in range(len(self.MainWnd_Button_List)):
-            self.MainWnd_Button_List[i].destroy()
-
         XmlProcess.SearchLegion = "2"
 
         self.TurnToSearchScene()
 
     def ButtonJejuInput(self):
-        for i in range(len(self.MainWnd_Button_List)):
-            self.MainWnd_Button_List[i].destroy()
-
         XmlProcess.SearchLegion = "3"
+
         self.TurnToSearchScene()
-    def SearchHorse(self):
-        pass
+
     def ButtonGmailSend(self):
         pass
 
     def ButtonTelegramSend(self):
         pass
 
-    def TurnToSearchScene(self):
-        self.MainWnd_Button_List.clear()
-        #self.photoBackIcon = PhotoImage(file="Photo/Back_icon.png")
-        #self.MainWnd_Button_List.append(Button(self.MainWnd, image=self.photoBackIcon, command=self.TurnToMainScene))
-        #self.MainWnd_Button_List[0].place(x=0,y=0)
-        Button(self.MainWnd, text="복귀", width=10, height=5, command=self.TurnToMainScene).place(x=200, y=200)
-        pass
-
-    def TurnToMainScene(self):
-      # for i in range(len(self.MainWnd_Button_List)):
-        #    self.MainWnd_Button_List[i].destroy()
-        self.MainSceneButtons()
 
 
 if __name__ == '__main__':
