a3c5520a021b13368752bfc35acf71281352787b
한영인 <gksduddls33@gmail.com>
2021-05-19 01:08:39 +0900

    0.0.7 서치씬 구현
    Canvas 만듬

--- MainGuI.py
+++ MainGuI.py
@@ -16,13 +16,13 @@ class MainGui:
 
         self.MainWnd_Button_List = []
         self.SearchObjectsList = []
-
+        self.SearchDateObjectsList = []
         self.photoGmail = PhotoImage(file="Photo/Gmail_icon.png")
         self.photoTelegram = PhotoImage(file="Photo/Telegram_icon.png")
         self.photoNamuwiki = PhotoImage(file="Photo/Namuwiki_icon.png")
 
         self.input_text = None
-
+        self.input_date = None
         self.MainSceneButtons()
 
         self.MainWnd.mainloop()
@@ -51,16 +51,42 @@ class MainGui:
         self.MainWnd_Button_List[5].place(x=190, y=580)
 
     def SetSearchButtons(self):
-        self.input_text = StringVar()
+        self.input_text = StringVar() # 경주마 이름 입력
         self.SearchObjectsList.append(Entry(self.MainWnd, textvariable=self.input_text, width=30))
-
         self.SearchObjectsList.append(Button(self.MainWnd, text="검색", command=self.SearchDef))
+        self.SearchObjectsList.append(Button(self.MainWnd, text="불러오기", command=self.LoadDataDef))
 
+        self.input_date = StringVar() # 경기날짜 입력
+        self.SearchObjectsList.append(Entry(self.MainWnd, textvariable=self.input_date, width=40))
+        self.SearchObjectsList.append(Button(self.MainWnd, text="검색", command=self.SearchDateDef))
+
+        self.SearchObjectsList.append(Button(self.MainWnd,text="다음 경기 예측하기",command=self.TenserFlow, width=64, height=3))
+
+        #self.SearchObjectsList.append(Canvas(self.MainWnd,bg='white',width=400,height=300))
+        self.Datacanvas = Canvas(self.MainWnd, bg='white', width=450, height=300)
+        self.Datacanvas.pack()
+        self.Graphcanvas = Canvas(self.MainWnd, bg='white', width=450, height=170)
+        self.Graphcanvas.pack()
         self.SearchObjectsPlace()
 
     def SearchObjectsPlace(self):
-        self.SearchObjectsList[0].place(x=0, y=110)
-        self.SearchObjectsList[1].place(x=250, y=110)
+        self.SearchObjectsList[0].place(x=0, y=50)
+        self.SearchObjectsList[1].place(x=220, y=49)
+        self.SearchObjectsList[2].place(x=260, y=49)
+        self.SearchObjectsList[3].place(x=0, y=650)
+        self.SearchObjectsList[4].place(x=290, y=649)
+        self.SearchObjectsList[5].place(x=10, y=400)
+        self.Datacanvas.place(x=10,y=90)
+        self.Graphcanvas.place(x=10, y=465)
+
+    def LoadDataDef(self):
+        pass
+
+    def TenserFlow(self):
+        pass
+
+    def SearchDateDef(self):
+        pass
 
     def SearchDef(self):
         Result = XmlProcess.SearchHorseProfile(self.input_text.get())
@@ -88,15 +114,18 @@ class MainGui:
 
         self.TurnToSearchScene()
 
+
     def ButtonBugyoungInput(self):
         XmlProcess.SearchLegion = "2"
 
         self.TurnToSearchScene()
 
+
     def ButtonJejuInput(self):
         XmlProcess.SearchLegion = "3"
 
         self.TurnToSearchScene()
+
 
     def ButtonGmailSend(self):
         pass
