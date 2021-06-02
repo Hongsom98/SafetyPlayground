20fe0a462c45d46de9b5c4b7a89a5aedc1e5410d
Hongsom98 <bfqut12@naver.com>
2021-06-02 21:02:15 +0900

    0.2.3 경기영상 크롤링
    개꿀 모듈 ㅋㅋ

--- test.py
+++ test.py
@@ -1,40 +1,9 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup as bs
-import pandas as pd
-from selenium.webdriver.common.keys import Keys
-import time
+from youtubesearchpython import *
 
-keyword = '(서울) 2021.05.29 7경주'
-url = 'https://www.youtube.com/results?search_query={}'.format(keyword)
+videosSearch = VideosSearch('(서울)2021.05.30+10경주', limit=10)
 
-driver = webdriver.Chrome('./chromedriver.exe')
-driver.get(url)
-soup = bs(driver.page_source, 'html.parser')
-driver.close()
-
-#name = soup.select('a#video-title')
-video_url = soup.select('a#video-title')
-#view = soup.select('a#video-title')
-
-
-url_list = []
-
-
-'''for i in range(len(name)):
-    name_list.append(name[i].text.strip())
-    view_list.append(view[i].get('aria-label').split()[-1])'''
-
-url_list.append('{}{}'.format('https://www.youtube.com', video_url[0].get('href')))
-
-'''youtubeDic = {
-    '제목': name_list,
-    '주소': url_list,
-    '조회수': view_list
-}'''
-
-#youtubeDf = pd.DataFrame(youtubeDic)
-
-print(url_list[0])
-
-#youtubeDf.to_csv('오마이걸유튜브.csv', encoding='', index=False)
+result = videosSearch.result()
+for i in range(10):
+    print(result['result'][i]['title'], end='->')
+    print(result['result'][i]['link'])
 
